6e02150d977a724d3a51d64ef4eb4c85da3562a6
Anton Medvedev <anton@elfet.ru>
2015-02-25 15:57:41 +0300

    Create constants.php

---
+++ src/constants.php
@@ -0,0 +1,9 @@
+<?php
+define('yes', true);
+define('ok', yes);
+define('okay', ok);
+define('no', false);
+define('not', no);
+define('maybe', (bool)rand(0, 1));
+define('perhaps', (bool)rand(0, 1));
+define('possibly', (bool)rand(0, 2));
